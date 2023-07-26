9515aae35681710067022add8c33493e5b3c0831
Nick Pateman <nick@npsoftware.co.uk>
2023-07-26 21:09:18 +0100

    chore: Test coverage of file system repo

--- Zombie.Api.UnitTests/Repositories/FileSystemRepositoryTests.cs
+++ Zombie.Api.UnitTests/Repositories/FileSystemRepositoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using Xunit;
+using Zombie.Api.Dto.Models;
 using Zombie.Api.Exceptions;
 using Zombie.Api.Repositories;
 using Zombie.Api.Services;
@@ -74,7 +75,7 @@ namespace Zombie.Api.UnitTests.Repositories
         }
 
         [Fact]
-        public async Task GivenKey_WhenGet_ThenDocumentParsed_AndSuccessReturned()
+        public async Task GivenKey_AndDocumentExists_WhenGet_ThenDocumentParsed_AndSuccessReturned()
         {
             // Arrange
             var options = new FileSystemRepositoryOptions
@@ -82,7 +83,7 @@ namespace Zombie.Api.UnitTests.Repositories
                 BasePath = "TestRepo"
             };
             var key = "Foo/Bar/FooBar.md";
-            var combinedPath = "TestRepo/Foo/Bar/FooBar.md";
+            var combinedPath = $"TestRepo/{key}";
             var mockIoService = new Mock<IIoService>();
             var mockDocumentParser = new Mock<IDocumentParser>();
 
@@ -130,7 +131,7 @@ namespace Zombie.Api.UnitTests.Repositories
         }
 
         [Fact]
-        public async void GivenKey_WhenDelete_ThenGetExistingDocument_AndDocumentDeleted()
+        public async void GivenKey_AndDocumentExists_WhenDelete_ThenGetExistingDocument_AndDocumentDeleted()
         {
             // Arrange
             var options = new FileSystemRepositoryOptions
@@ -138,7 +139,7 @@ namespace Zombie.Api.UnitTests.Repositories
                 BasePath = "TestRepo"
             };
             var key = "Foo/DeleteTest.md";
-            var combinedPath = "TestRepo/Foo/DeleteTest.md";
+            var combinedPath = $"TestRepo/{key}";
             var mockIoService = new Mock<IIoService>();
             var mockDocumentParser = new Mock<IDocumentParser>();
 
@@ -172,6 +173,276 @@ namespace Zombie.Api.UnitTests.Repositories
                 Times.Exactly(2));
             mockIoService.Verify(x => x.Exists(
                 It.Is<string>(y => y == combinedPath)), Times.Once);
+        }
+
+        [Fact]
+        public async void GivenKey_AndDocumentNotExists_WhenDelete_ThenNoFileDeleted_AndNotFoundReturned()
+        {
+            // Arrange
+            var options = new FileSystemRepositoryOptions
+            {
+                BasePath = "TestRepo"
+            };
+            var key = "Foo/DoesNotExist.md";
+            var combinedPath = $"TestRepo/{key}";
+            var mockIoService = new Mock<IIoService>();
+            var mockDocumentParser = new Mock<IDocumentParser>();
+
+            mockIoService.Setup(x => x.CheckPathIsWritable(
+                It.IsAny<string>()))
+                .Returns(true);
+
+            var sut = new FileSystemDocumentRepository(
+                options,
+                mockIoService.Object,
+                mockDocumentParser.Object);
+
+            mockIoService.Setup(x => x.CombinePath(
+                It.IsAny<string[]>())).Returns(combinedPath);
+
+            mockIoService.Setup(x => x.Exists(
+                It.IsAny<string>()))
+                .Returns(false);
+
+            // Act
+            var result = await sut.Delete(key);
+
+            // Assert
+            Assert.Equal(Api.Repositories.Enums.Status.NotFound, result.Status);
+            mockIoService.Verify(x => x.CheckPathIsWritable(
+                It.Is<string>(y => y == options.BasePath)), Times.Once);
+            mockIoService.Verify(x => x.Delete(
+                It.Is<string>(y => y == combinedPath)), Times.Never);
+            mockIoService.Verify(x => x.CombinePath(
+                It.Is<string>(y => y == options.BasePath),
+                It.Is<string>(y => y == key)),
+                Times.Exactly(1));
+            mockIoService.Verify(x => x.Exists(
+                It.Is<string>(y => y == combinedPath)), Times.Once);
+        }
+
+        [Fact]
+        public async void GivenDocument_AndKeyMissing_WhenDelete_ThenArgumentExceptionThrown()
+        {
+            // Arrange
+            var options = new FileSystemRepositoryOptions
+            {
+                BasePath = "TestRepo"
+            };
+            var mockIoService = new Mock<IIoService>();
+            var mockDocumentParser = new Mock<IDocumentParser>();
+
+            mockIoService.Setup(x => x.CheckPathIsWritable(
+                It.IsAny<string>()))
+                .Returns(true);
+
+            var sut = new FileSystemDocumentRepository(
+                options,
+                mockIoService.Object,
+                mockDocumentParser.Object);
+
+            // Act
+            await Assert.ThrowsAnyAsync<ArgumentException>(async () =>
+            {
+                _ = await sut.Delete(new Document());
+            });
+
+            // Assert
+            mockIoService.Verify(x => x.CheckPathIsWritable(
+                It.Is<string>(y => y == options.BasePath)), Times.Once);
+        }
+
+        [Fact]
+        public async void GivenDocument_AndDocumentExists_WhenDelete_ThenGetExistingDocument_AndDocumentDeleted()
+        {
+            // Arrange
+            var options = new FileSystemRepositoryOptions
+            {
+                BasePath = "TestRepo"
+            };
+            var key = "Foo/DeleteTest.md";
+            var combinedPath = $"TestRepo/{key}";
+            var mockIoService = new Mock<IIoService>();
+            var mockDocumentParser = new Mock<IDocumentParser>();
+
+            mockIoService.Setup(x => x.CheckPathIsWritable(
+                It.IsAny<string>()))
+                .Returns(true);
+
+            var sut = new FileSystemDocumentRepository(
+                options,
+                mockIoService.Object,
+                mockDocumentParser.Object);
+
+            mockIoService.Setup(x => x.CombinePath(
+                It.IsAny<string[]>())).Returns(combinedPath);
+
+            mockIoService.Setup(x => x.Exists(
+                It.IsAny<string>()))
+                .Returns(true);
+
+            // Act
+            var result = await sut.Delete(new Document { Key = key });
+
+            // Assert
+            mockIoService.Verify(x => x.CheckPathIsWritable(
+                It.Is<string>(y => y == options.BasePath)), Times.Once);
+            mockIoService.Verify(x => x.Delete(
+                It.Is<string>(y => y == combinedPath)), Times.Once);
+            mockIoService.Verify(x => x.CombinePath(
+                It.Is<string>(y => y == options.BasePath),
+                It.Is<string>(y => y == key)),
+                Times.Exactly(2));
+            mockIoService.Verify(x => x.Exists(
+                It.Is<string>(y => y == combinedPath)), Times.Once);
+        }
+
+        [Fact]
+        public async void GivenDocument_AndDocumentNotExists_WhenDelete_ThenNoFileDeleted_AndNotFoundReturned()
+        {
+            // Arrange
+            var options = new FileSystemRepositoryOptions
+            {
+                BasePath = "TestRepo"
+            };
+            var key = "Foo/DoesNotExist.md";
+            var combinedPath = $"TestRepo/{key}";
+            var mockIoService = new Mock<IIoService>();
+            var mockDocumentParser = new Mock<IDocumentParser>();
+
+            mockIoService.Setup(x => x.CheckPathIsWritable(
+                It.IsAny<string>()))
+                .Returns(true);
+
+            var sut = new FileSystemDocumentRepository(
+                options,
+                mockIoService.Object,
+                mockDocumentParser.Object);
+
+            mockIoService.Setup(x => x.CombinePath(
+                It.IsAny<string[]>())).Returns(combinedPath);
+
+            mockIoService.Setup(x => x.Exists(
+                It.IsAny<string>()))
+                .Returns(false);
+
+            // Act
+            var result = await sut.Delete(new Document { Key = key });
+
+            // Assert
+            Assert.Equal(Api.Repositories.Enums.Status.NotFound, result.Status);
+            mockIoService.Verify(x => x.CheckPathIsWritable(
+                It.Is<string>(y => y == options.BasePath)), Times.Once);
+            mockIoService.Verify(x => x.Delete(
+                It.Is<string>(y => y == combinedPath)), Times.Never);
+            mockIoService.Verify(x => x.CombinePath(
+                It.Is<string>(y => y == options.BasePath),
+                It.Is<string>(y => y == key)),
+                Times.Exactly(1));
+            mockIoService.Verify(x => x.Exists(
+                It.Is<string>(y => y == combinedPath)), Times.Once);
+        }
+
+        [Fact]
+        public async Task GivenDocument_AndUniqueKey_WhenInsert_ThenDocumentInserted()
+        {
+            // Arrange
+            var options = new FileSystemRepositoryOptions
+            {
+                BasePath = "TestRepo"
+            };
+            var key = $"Foo/{Guid.NewGuid()}.md";
+            var combinedPath = $"TestRepo/{key}";
+            var content = "Hello World!";
+            var mockIoService = new Mock<IIoService>();
+            var mockDocumentParser = new Mock<IDocumentParser>();
+            var document = new Document
+            {
+                Key = key
+            };
+
+            mockIoService.Setup(x => x.CheckPathIsWritable(
+                It.IsAny<string>()))
+                .Returns(true);
+
+            var sut = new FileSystemDocumentRepository(
+                options,
+                mockIoService.Object,
+                mockDocumentParser.Object);
+
+            mockIoService.Setup(x => x.CombinePath(
+                It.IsAny<string[]>())).Returns(combinedPath);
+
+            mockDocumentParser.Setup(x => x.Serialise(
+                It.IsAny<Document>()))
+                .Returns(content);
+
+            // Act
+            var result = await sut.Insert(document);
+
+            // Assert
+            Assert.Equal(Api.Repositories.Enums.Status.Success, result.Status);
+            mockIoService.Verify(x => x.CombinePath(
+                It.Is<string>(y => y == options.BasePath),
+                It.Is<string>(y => y == key)),
+                Times.Once);
+            mockIoService.Verify(x => x.Exists(
+                It.Is<string>(y => y == combinedPath)), Times.Once);
+            mockDocumentParser.Verify(x => x.Serialise(
+                It.Is<Document>(y => y == document)), Times.Once);
+            mockIoService.Verify(x => x.WriteAllTextAsync(
+                It.Is<string>(y => y == combinedPath),
+                It.Is<string>(y => y == content)), Times.Once);
+        }
+
+        [Fact]
+        public async Task GivenDocument_AndConflict_WhenInsert_ThenConflictReturned()
+        {
+            // Arrange
+            var options = new FileSystemRepositoryOptions
+            {
+                BasePath = "TestRepo"
+            };
+            var key = $"Foo/{Guid.NewGuid()}.md";
+            var combinedPath = $"TestRepo/{key}";
+            var content = "Hello World!";
+            var mockIoService = new Mock<IIoService>();
+            var mockDocumentParser = new Mock<IDocumentParser>();
+            var document = new Document
+            {
+                Key = key
+            };
+
+            mockIoService.Setup(x => x.CheckPathIsWritable(
+                It.IsAny<string>()))
+                .Returns(true);
+
+            var sut = new FileSystemDocumentRepository(
+                options,
+                mockIoService.Object,
+                mockDocumentParser.Object);
+
+            mockIoService.Setup(x => x.CombinePath(
+                It.IsAny<string[]>())).Returns(combinedPath);
+
+            mockIoService.Setup(x => x.Exists(
+                It.IsAny<string>())).Returns(true);
+
+            mockDocumentParser.Setup(x => x.Serialise(
+                It.IsAny<Document>()))
+                .Returns(content);
+
+            // Act
+            var result = await sut.Insert(document);
+
+            // Assert
+            Assert.Equal(Api.Repositories.Enums.Status.Conflict, result.Status);
+            mockIoService.Verify(x => x.CombinePath(
+                It.Is<string>(y => y == options.BasePath),
+                It.Is<string>(y => y == key)),
+                Times.Once);
+            mockIoService.Verify(x => x.Exists(
+                It.IsAny<string>()), Times.Once);
         }
     }
 }
--- Zombie.Api.UnitTests/Services/FileSystemIoServiceTests.cs
+++ Zombie.Api.UnitTests/Services/FileSystemIoServiceTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 using Zombie.Api.Services;
 
 namespace Zombie.Api.UnitTests.Services
@@ -25,6 +20,92 @@ namespace Zombie.Api.UnitTests.Services
 
             // Assert
             Assert.Equal(expectedCombinedPath, result);
+        }
+
+        [Fact]
+        public async Task GivenExistingFile_WhenExists_ThenTrueReturned()
+        {
+            // Act
+            var path = $"{Guid.NewGuid()}.tmp";
+            var sut = new FileSystemIoService();
+            await File.WriteAllTextAsync(path, "Hello World!");
+
+            // Arrange
+            var exists = sut.Exists(path);
+
+            // Assert
+            Assert.True(exists);
+
+            // Cleanup
+            File.Delete(path);
+        }
+
+        [Fact]
+        public void GivenNonExistantFile_WhenExists_ThenFalseReturned()
+        {
+            // Act
+            var path = $"{Guid.NewGuid()}.tmp";
+            var sut = new FileSystemIoService();
+
+            // Arrange
+            var exists = sut.Exists(path);
+
+            // Assert
+            Assert.False(exists);
+        }
+
+        [Fact]
+        public async Task GivenExistingFile_WhenReadAllTextAsync_ThenFileDataReturned()
+        {
+            // Act
+            var content = "Hello World!";
+            var path = $"{Guid.NewGuid()}.tmp";
+            var sut = new FileSystemIoService();
+            await File.WriteAllTextAsync(path, content);
+
+            // Arrange
+            var data = await sut.ReadAllTextAsync(path);
+
+            // Assert
+            Assert.Equal(content, data);
+
+            // Cleanup
+            File.Delete(path);
+        }
+
+        [Fact]
+        public async Task GivenNewFile_AndContent_WhenWriteAllTextAsync_ThenContentWrittentToFile()
+        {
+            // Act
+            var content = "Hello World!";
+            var path = $"{Guid.NewGuid()}.tmp";
+            var sut = new FileSystemIoService();
+
+            // Arrange
+            await sut.WriteAllTextAsync(path, content);
+
+            // Assert
+            var data = await File.ReadAllTextAsync(path);
+            Assert.Equal(content, data);
+
+            // Cleanup
+            File.Delete(path);
+        }
+
+        [Fact]
+        public async Task GivenExistingFile_WhenDelete_ThenFileDeleted()
+        {
+            // Act
+            var content = "Hello World!";
+            var path = $"{Guid.NewGuid()}.tmp";
+            var sut = new FileSystemIoService();
+            await File.WriteAllTextAsync(path, content);
+
+            // Arrange
+            sut.Delete(path);
+
+            // Assert
+            Assert.False(File.Exists(path));
         }
     }
 }
